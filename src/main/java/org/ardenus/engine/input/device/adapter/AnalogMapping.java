1eec3f721f9163ca1373fad1d263501eba645e2a
Whirvis Ardenaur <whirvis@ardenus.org>
2021-09-19 00:22:17 -0500

    Remove unnecessary Inherited in ButtonMapping and AnalogMapping

--- src/main/java/org/ardenus/engine/input/device/adapter/AnalogMapping.java
+++ src/main/java/org/ardenus/engine/input/device/adapter/AnalogMapping.java
@@ -2,7 +2,6 @@ package org.ardenus.engine.input.device.adapter;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -14,7 +13,6 @@ import java.lang.annotation.Target;
  * 
  * @see DeviceAdapter#map(MappedAnalog)
  */
-@Inherited
 @Documented
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
