796859a2ce058785a269e7bd6bf9737066f1f42f
Whirvis Ardenaur <whirvis@ardenus.org>
2021-11-22 17:50:11 -0600

    Add DualShock 3 adapter mappings

---
+++ src/main/java/com/whirvis/kibasan/adapter/dualshock/Ds3RumbleMapping.java
@@ -0,0 +1,12 @@
+package com.whirvis.kibasan.adapter.dualshock;
+
+import com.whirvis.kibasan.adapter.RumbleMapping;
+import com.whirvis.kibasan.feature.RumbleMotor;
+
+public class Ds3RumbleMapping extends RumbleMapping {
+
+	public Ds3RumbleMapping(RumbleMotor motor) {
+		super(motor);
+	}
+
+}
