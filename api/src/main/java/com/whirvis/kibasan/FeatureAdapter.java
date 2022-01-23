8eee14e0d081b95d8b330fc7091acc7677f8ac93
Whirvis Ardenaur <whirvis@ardenus.org>
2022-01-23 17:30:22 -0600

    Add @FeatureAdapter annotation

---
+++ api/src/main/java/com/whirvis/kibasan/FeatureAdapter.java
@@ -0,0 +1,21 @@
+package com.whirvis.kibasan;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * When present, indicates a method is used as a device feature adapter.
+ * Feature adapters are used to fetch input device information and send
+ * output data. Use of this annotation is optional, but recommended as
+ * it improves code readability.
+ *
+ * @see StateUpdater
+ */
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface FeatureAdapter {
+}
