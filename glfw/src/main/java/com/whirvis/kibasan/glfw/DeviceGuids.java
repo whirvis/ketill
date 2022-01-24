be16d98e29b7d74426914c464d3cc8ee7ca69f54
Whirvis Ardenaur <whirvis@ardenus.org>
2022-01-23 20:33:20 -0600

    Bring DeviceGuids up to new format specs

--- glfw/src/main/java/com/whirvis/kibasan/glfw/DeviceGuids.java
+++ glfw/src/main/java/com/whirvis/kibasan/glfw/DeviceGuids.java
@@ -1,39 +1,27 @@
 package com.whirvis.kibasan.glfw;
 
-import java.util.Objects;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-/**
- * A container for device GUIDs.
- * 
- * @see #getGuids(String, OperatingSystem)
- */
 public interface DeviceGuids {
 
-	/**
-	 * @param id
-	 *            the input device ID.
-	 * @param os
-	 *            the operating system ID.
-	 * @return the GUIDs for device with {@code id} on {@code os}, {@code null}
-	 *         if none exist.
-	 * @throws NullPointerException
-	 *             if {@code id} or {@code os} are {@code null}.
-	 */
-	public Iterable<String> getGuids(String id, String os);
+    /**
+     * @param id the input device ID.
+     * @param os the operating system ID.
+     * @return the GUIDs for device with {@code id} when running on {@code os},
+     * {@code null} if no such device exists.
+     */
+    @Nullable Iterable<String> getGuids(@NotNull String id, @NotNull String os);
 
-	/**
-	 * @param id
-	 *            the input device ID.
-	 * @param os
-	 *            the operating system.
-	 * @return the GUIDs for device with {@code id} on {@code os}, {@code null}
-	 *         if none exist.
-	 * @throws NullPointerException
-	 *             if {@code id} or {@code os} are {@code null}.
-	 */
-	public default Iterable<String> getGuids(String id, OperatingSystem os) {
-		Objects.requireNonNull(os, "os");
-		return this.getGuids(id, os.id);
-	}
+    /**
+     * @param id the input device ID.
+     * @param os the operating system.
+     * @return the GUIDs for device with {@code id} when running on {@code os},
+     * {@code null} if no such device exists.
+     */
+    default @Nullable Iterable<String> getGuids(@NotNull String id,
+                                                @NotNull OperatingSystem os) {
+        return this.getGuids(id, os.id);
+    }
 
 }
