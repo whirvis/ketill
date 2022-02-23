bda086ad18ef173839026b597db81b60bb35fa78
Whirvis Ardenaur <whirvis@ardenus.org>
2022-02-23 11:12:25 -0600

    Explain tests in DeviceInfoTest.java

--- adapters/hid-usb-api/src/test/java/io/ketill/hidusb/DeviceInfoTest.java
+++ adapters/hid-usb-api/src/test/java/io/ketill/hidusb/DeviceInfoTest.java
@@ -11,23 +11,27 @@ class DeviceInfoTest {
     @Test
     void __init__() {
         /*
-         * TODO: explain
+         * Vendor IDs are an unsigned short. Any value outside
+         * the range of 0x0000 to 0xFFFF is out of bounds.
          */
         assertThrows(IllegalArgumentException.class,
                 () -> new DeviceInfo(Integer.MIN_VALUE, 0x0000));
         assertThrows(IllegalArgumentException.class,
-                () -> new DeviceInfo(0x0000, Integer.MIN_VALUE));
+                () -> new DeviceInfo(Integer.MAX_VALUE, 0x0000));
 
         /*
-         * TODO: explain
+         * Product IDs are an unsigned short. Any value outside
+         * the range of 0x0000 to 0xFFFF is out of bounds.
          */
         assertThrows(IllegalArgumentException.class,
-                () -> new DeviceInfo(Integer.MAX_VALUE, 0x0000));
+                () -> new DeviceInfo(0x0000, Integer.MIN_VALUE));
         assertThrows(IllegalArgumentException.class,
                 () -> new DeviceInfo(0x0000, Integer.MAX_VALUE));
 
         /*
-         * TODO: explain
+         * Any randomly generated number within a range 0x0000 to
+         * 0xFFFF should not result in an exception being thrown
+         * when constructing an instance of DeviceInfo.
          */
         Random random = new Random();
         int vendorId = random.nextInt(0xFFFF);
