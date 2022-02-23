69ebc5fe2f0be4f980688d7ac432c07af71682b2
Whirvis Ardenaur <whirvis@ardenus.org>
2022-02-22 22:22:09 -0600

    Update AssertUtils.java

--- adapters/hid-usb-api/src/test/java/io/ketill/hidusb/AssertUtils.java
+++ adapters/hid-usb-api/src/test/java/io/ketill/hidusb/AssertUtils.java
@@ -6,8 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AssertUtils {
 
-    static void assertThrowsCause(Throwable cause,
-                                  Executable executable) {
+    static void assertThrowsCause(Throwable cause, Executable executable) {
         Throwable caught = null;
         try {
             executable.execute();
