2fccf6bdc8037944bf521859c4eb1a496506279b
Whirvis Ardenaur <whirvis@ardenus.org>
2022-03-01 20:33:20 -0600

    Update MockGlfwDeviceAdapter.java

--- adapters/glfw-api/src/test/java/io/ketill/glfw/MockGlfwDeviceAdapter.java
+++ adapters/glfw-api/src/test/java/io/ketill/glfw/MockGlfwDeviceAdapter.java
@@ -2,10 +2,12 @@ package io.ketill.glfw;
 
 import io.ketill.IoDevice;
 import io.ketill.MappedFeatureRegistry;
+import org.jetbrains.annotations.NotNull;
 
 class MockGlfwDeviceAdapter extends GlfwDeviceAdapter<IoDevice> {
 
-    MockGlfwDeviceAdapter(IoDevice device, MappedFeatureRegistry registry,
+    MockGlfwDeviceAdapter(@NotNull IoDevice device,
+                          @NotNull MappedFeatureRegistry registry,
                           long ptr_glfwWindow) {
         super(device, registry, ptr_glfwWindow);
     }
