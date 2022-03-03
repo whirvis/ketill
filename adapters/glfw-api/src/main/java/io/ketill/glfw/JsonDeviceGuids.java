34f16a2256a22e943b64ce1ce7a9c6fdd940c5a7
Whirvis Ardenaur <whirvis@ardenus.org>
2022-03-02 18:36:31 -0600

    Create JsonDeviceGuidsTest in glfw-api module

--- adapters/glfw-api/src/main/java/io/ketill/glfw/JsonDeviceGuids.java
+++ adapters/glfw-api/src/main/java/io/ketill/glfw/JsonDeviceGuids.java
@@ -1,8 +1,9 @@
 package io.ketill.glfw;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -13,68 +14,144 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 
 /**
  * A JSON based container for the GUIDs of a device.
  * <p>
- * JSON containers must have a JSON object with the key {@code "guids"}.
- * This object contains arrays of GUIDs, with their key being the ID of
- * the corresponding operating system.
+ * These JSON containers consist of one or more JSON arrays. The arrays in
+ * question contain the GUIDs of a device when connected to a specific OS.
+ * The keys of these arrays correspond to the operating system ID.
  * <p>
  * The following is an example of a JSON container:
  * <pre>
  * {
- *  "guids": {
- *    "windows": [
- *      "03000000ad1b000016f0000000000000",
- *      "030000000d0f00006300000000000000",
- *      "030000005e040000130b000000000000"
- *    ],
- *    "linux": [
- *      "030000005e0400008e02000010010000",
- *      "030000005e0400000a0b000005040000",
- *      "030000005e040000120b000001050000"
- *    ],
- *    "mac_osx": [
- *      "030000005e0400008e02000000000000",
- *      "030000005e040000130b000001050000",
- *      "030000005e040000200b000011050000"
- *    ]
- *  }
+ *   "windows": [
+ *     "03000000ad1b000016f0000000000000",
+ *     "030000000d0f00006300000000000000",
+ *     "030000005e040000130b000000000000"
+ *   ],
+ *   "mac_osx": [
+ *     "030000005e0400008e02000000000000",
+ *     "030000005e040000130b000001050000",
+ *     "030000005e040000200b000011050000"
+ *   ],
+ *   "linux": [
+ *     "030000005e0400008e02000010010000",
+ *     "030000005e0400000a0b000005040000",
+ *     "030000005e040000120b000001050000"
+ *   ]
  * }
  * </pre>
  */
 public class JsonDeviceGuids extends DeviceGuids {
 
-    private static final Gson GSON = new GsonBuilder().create();
-
     /**
      * Loads the device GUIDs of a JSON file from an input stream.
+     * <p>
+     * For this method to successfully return, {@code in} must contain a
+     * valid JSON device GUIDs container. If the contained JSON does not
+     * follow specifications, a {@code JsonIOException} will be thrown.
+     * <p>
+     * An example of a valid container is given in the class JavaDocs.
      *
      * @param in the input stream to read from.
      * @return the loaded device GUIDs.
+     * @throws NullPointerException if {@code in} is {@code null}.
+     * @throws JsonIOException      if {@code in} contains invalid JSON
+     *                              for a JSON device GUIDs container.
      */
     public static @NotNull DeviceGuids load(@NotNull InputStream in) {
         Objects.requireNonNull(in, "in");
+
+        JsonDeviceGuids guids = new JsonDeviceGuids();
         InputStreamReader isr = new InputStreamReader(in);
+
+        /*
+         * The root JSON element is expected to be a JSON object.
+         * This is so GSON can load the contained "guids" object
+         * and convert it to a Java map instance.
+         */
         JsonElement json = JsonParser.parseReader(isr);
-        return GSON.fromJson(json, JsonDeviceGuids.class);
+        if (!json.isJsonObject()) {
+            throw new JsonIOException("expected JSON object");
+        }
+
+        /*
+         * If the GUIDs container has no systems... what is it
+         * doing here? This was likely user oversight. Throw an
+         * exception in hopes of preventing an annoying bug.
+         */
+        JsonObject jsonObj = json.getAsJsonObject();
+        if (jsonObj.entrySet().isEmpty()) {
+            throw new JsonIOException("no systems present");
+        }
+
+        for (String systemId : jsonObj.keySet()) {
+            /*
+             * Each system must contain one or more GUIDs for the
+             * device. For simplicity, require each system store
+             * their GUIDs in a JSON array.
+             */
+            JsonElement systemGuids = jsonObj.get(systemId);
+            if (!systemGuids.isJsonArray()) {
+                throw new JsonIOException("\"" + systemId + "\" must be a " + "JSON array");
+            }
+
+            /*
+             * If the system has no specified GUIDs... what is it
+             * doing here? This was likely a user oversight. Throw
+             * an exception in hope of preventing an annoying bug.
+             */
+            JsonArray systemGuidsArr = systemGuids.getAsJsonArray();
+            if (systemGuidsArr.isEmpty()) {
+                throw new JsonIOException("\"" + systemId + "\" has no " +
+                        "device" + " GUIDs");
+            }
+
+            /*
+             * Each element in this JSON array must be a string.
+             * Different value types indicates the user likely
+             * has a misunderstanding of how GUIDs are stored.
+             */
+            List<String> systemGuidsList = new ArrayList<>();
+            for (int i = 0; i < systemGuidsArr.size(); i++) {
+                JsonElement guid = systemGuidsArr.get(i);
+                if (!guid.isJsonPrimitive() || !guid.getAsJsonPrimitive().isString()) {
+                    throw new JsonIOException("element " + i + " of \"" + systemId + "\" must be a string");
+                }
+                systemGuidsList.add(guid.getAsString());
+            }
+
+            guids.guids.put(systemId, systemGuidsList);
+        }
+
+        return guids;
     }
 
     /**
      * Loads device GUIDs from a JSON file in the classpath.
      * <p>
-     * This method is a shorthand for {@link #load(InputStream)}, with the
-     * argument for {@code in} being the URL of {@code path} opened as an
-     * input stream using {@link URL#openStream()}.
+     * For this method to successfully return, the resource must contain
+     * a valid JSON device GUIDs container. If the JSON contained in the
+     * resource does not follow specifications, a {@code JsonIOException}
+     * will be thrown.
+     * <p>
+     * An example of a valid container is given in the class JavaDocs.
      *
      * @param path the path of the resource to read from.
      * @return the loaded device GUIDs.
-     * @throws IOException if an I/O error occurs.
+     * @throws NullPointerException if {@code path} is {@code null}.
+     * @throws JsonIOException      if the resource at {@code path}
+     *                              contains invalid JSON for a JSON
+     *                              device GUIDs container.
+     * @throws IOException          if an I/O error occurs.
      */
     /* @formatter:off */
     public static @NotNull DeviceGuids
@@ -82,7 +159,7 @@ public class JsonDeviceGuids extends DeviceGuids {
         Objects.requireNonNull(path, "path");
         URL url = DeviceGuids.class.getResource(path);
         if (url == null) {
-            throw new IOException("no such resource");
+            throw new IOException("no such resource " + path);
         }
         return load(url.openStream());
     }
@@ -91,32 +168,50 @@ public class JsonDeviceGuids extends DeviceGuids {
     /**
      * Loads device GUIDs from a JSON file.
      * <p>
-     * This method is a shorthand for {@link #load(InputStream)}, with the
-     * argument for {@code in} being {@code new FileInputStream(file)}.
+     * For this method to successfully return, the contents of {@code file}
+     * must contain a valid JSON device GUIDs container. If the contents
+     * of the file do not follow specifications, a {@code JsonIOException}
+     * will be thrown.
+     * <p>
+     * An example of a valid container is given in the class JavaDocs.
      *
      * @param file the file to read from.
      * @return the loaded device GUIDs.
-     * @throws IOException          if an I/O error occurs.
      * @throws NullPointerException if {@code file} is {@code null}.
+     * @throws JsonIOException      if the contents of {@code file}
+     *                              contains invalid JSON for a JSON
+     *                              device GUIDs container.
+     * @throws IOException          if an I/O error occurs.
      */
     /* @formatter:off */
     public static @NotNull DeviceGuids
             loadFile(@NotNull File file) throws IOException {
         Objects.requireNonNull(file, "file");
-        return load(new FileInputStream(file));
+
+        FileInputStream in = new FileInputStream(file);
+        DeviceGuids guids = load(in);
+        in.close(); /* prevent memory leak */
+
+        return guids;
     }
     /* @formatter:on */
 
     /**
      * Loads device GUIDs from a JSON file.
      * <p>
-     * This method is a shorthand for {@link #loadFile(File)}, with the
-     * argument for {@code file} being {@code new File(path)}.
+     * For this method to successfully return, the file at {@code path} must
+     * contain a valid JSON device GUIDs container. If the file's contents do
+     * not follow specifications, a {@code JsonIOException} will be thrown.
+     * <p>
+     * An example of a valid container is given in the class JavaDocs.
      *
      * @param path the path of the file to read from.
      * @return the loaded device GUIDs.
-     * @throws IOException          if an I/O error occurs.
      * @throws NullPointerException if {@code path} is {@code null}.
+     * @throws JsonIOException      if the contents of the file located
+     *                              {@code path} contains invalid JSON
+     *                              for a JSON device GUIDs container.
+     * @throws IOException          if an I/O error occurs.
      */
     /* @formatter:off */
     public static @NotNull DeviceGuids
@@ -126,21 +221,19 @@ public class JsonDeviceGuids extends DeviceGuids {
     }
     /* @formatter:on */
 
-    /* loaded by GSON */
-    @SuppressWarnings("all")
-    private Map<String, Set<String>> guids;
+    private final Map<String, List<String>> guids;
 
     private JsonDeviceGuids() {
-        /* require initialization via load() */
+        this.guids = new HashMap<>();
     }
 
     /* @formatter:off */
     @Override
-    protected @Nullable Set<@NotNull String>
+    protected @Nullable Collection<@NotNull String>
             getGuidsImpl(@NotNull String systemId) {
-        Set<String> osGuids = guids.get(systemId);
+        List<String> osGuids = guids.get(systemId);
         if (osGuids != null && !osGuids.isEmpty()) {
-            return Collections.unmodifiableSet(osGuids);
+            return Collections.unmodifiableList(osGuids);
         }
         return null;
     }
---
+++ adapters/glfw-api/src/test/java/io/ketill/glfw/JsonDeviceGuidsTest.java
@@ -0,0 +1,217 @@
+package io.ketill.glfw;
+
+import com.google.gson.JsonIOException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SuppressWarnings("ConstantConditions")
+class JsonDeviceGuidsTest {
+
+    private static InputStream getResource(String path) throws IOException {
+        String fullPath = "/json_device_guids/" + path;
+        URL url = JsonDeviceGuidsTest.class.getResource(fullPath);
+        if (url == null) {
+            throw new IOException("no such resource " + fullPath);
+        }
+        return url.openStream();
+    }
+
+    private static void assertResourceThrows(String path) {
+        assertThrows(JsonIOException.class, () -> {
+            InputStream in = getResource(path);
+            JsonDeviceGuids.load(in);
+            in.close(); /* technically redundant */
+        });
+    }
+
+    private static DeviceGuids loadValid() throws IOException {
+        InputStream in = getResource("valid.json");
+        DeviceGuids guids = JsonDeviceGuids.load(in);
+        in.close(); /* cleanup resource */
+        return guids;
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static File copyResource(String path) throws IOException {
+        InputStream in = getResource(path);
+
+        /* generate random name for temp file */
+        Random random = new Random();
+        String name = Integer.toHexString(random.nextInt());
+        File temp = new File(name + ".temp");
+        FileOutputStream tempOut = new FileOutputStream(temp);
+
+        /* copying in chunks is faster */
+        int read;
+        byte[] buf = new byte[1024];
+        while ((read = in.read(buf)) > 0) {
+            tempOut.write(buf, 0, read);
+        }
+
+        /* prevent memory leak */
+        in.close();
+        tempOut.close();
+
+        return temp;
+    }
+
+    @Test
+    void load() throws IOException {
+        /*
+         * It would not make sense to attempt loading a JSON device
+         * GUIDs container from a null input stream. As such, assume
+         * this was a mistake by the user and thrown an exception.
+         */
+        assertThrows(NullPointerException.class,
+                () -> JsonDeviceGuids.load(null));
+
+        /*
+         * The root JSON element is expected to be a JSON object.
+         * This is so GSON can load the contained "guids" object
+         * and convert it to a Java map instance.
+         */
+        assertResourceThrows("illegal_root_type.json");
+
+        /*
+         * If the GUIDs container has no systems... what is it
+         * doing here? This was likely user oversight. Throw an
+         * exception in hopes of preventing an annoying bug.
+         */
+        assertResourceThrows("no_systems.json");
+
+        /*
+         * Each system must contain one or more GUIDs for the
+         * device. For simplicity, require each system store
+         * their GUIDs in a JSON array.
+         */
+        assertResourceThrows("illegal_system_type.json");
+
+        /*
+         * If the system has no specified GUIDs... what is it
+         * doing here? This was likely a user oversight. Throw
+         * an exception in hope of preventing an annoying bug.
+         */
+        assertResourceThrows("no_device_guids.json");
+
+        /*
+         * Each element in this JSON array must be a string.
+         * Different value types indicates the user likely
+         * has a misunderstanding of how GUIDs are stored.
+         */
+        assertResourceThrows("illegal_guid_type.json");
+
+        /*
+         * Now that each invalid scenario has been confirmed
+         * to throw an exception (as it should), ensure that
+         * a valid JSON device GUID container does not cause
+         * an exception to be thrown.
+         */
+        DeviceGuids guids = loadValid();
+
+        /*
+         * When a requested system is not present in the JSON
+         * device GUIDs container, null must be returned. This
+         * is to comply with DeviceGuids specs.
+         */
+        assertNull(guids.getGuids("ios"));
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void loadWindows() throws IOException {
+        DeviceGuids guids = loadValid();
+
+        List<String> windowsGuids = new ArrayList<>();
+        windowsGuids.add("03000000ad1b000016f0000000000000");
+        windowsGuids.add("030000000d0f00006300000000000000");
+        windowsGuids.add("030000005e040000130b000000000000");
+
+        assertIterableEquals(windowsGuids, guids.getSystemGuids());
+    }
+
+    @Test
+    @EnabledOnOs(OS.MAC)
+    void loadMacOSX() throws IOException {
+        DeviceGuids guids = loadValid();
+
+        List<String> macGuids = new ArrayList<>();
+        macGuids.add("030000005e0400008e02000000000000");
+        macGuids.add("030000005e040000130b000001050000");
+        macGuids.add("030000005e040000200b000011050000");
+
+        assertIterableEquals(macGuids, guids.getSystemGuids());
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void loadLinux() throws IOException {
+        DeviceGuids guids = loadValid();
+
+        List<String> linuxGuids = new ArrayList<>();
+        linuxGuids.add("030000005e0400008e02000010010000");
+        linuxGuids.add("030000005e0400000a0b000005040000");
+        linuxGuids.add("030000005e040000120b000001050000");
+
+        assertIterableEquals(linuxGuids, guids.getSystemGuids());
+    }
+
+    @Test
+    void loadResource() {
+        /*
+         * It would not make sense to attempt loading a JSON device
+         * GUIDs container from a null resource. Assume this was a
+         * mistake by the user and throw an exception.
+         */
+        assertThrows(NullPointerException.class,
+                () -> JsonDeviceGuids.loadResource(null));
+
+        /*
+         * However, if the resource does not exist, an IOException
+         * should be thrown. NullPointerException is not thrown in
+         * this instance as it leads the user to think a variable
+         * is null (when in reality a resource is not present).
+         */
+        assertThrows(IOException.class, () -> JsonDeviceGuids.loadResource(
+                "not_exist.json"));
+
+        assertDoesNotThrow(() -> {
+            String validPath = "/json_device_guids/valid.json";
+            JsonDeviceGuids.loadResource(validPath);
+        });
+    }
+
+    @Test
+    void loadFile() throws IOException {
+        /*
+         * It would not make sense to attempt loading a JSON device
+         * GUIDs container from a null file (or file path). Assume
+         * this was a mistake by the user and throw an exception.
+         */
+        assertThrows(NullPointerException.class,
+                () -> JsonDeviceGuids.loadFile((File) null));
+        assertThrows(NullPointerException.class,
+                () -> JsonDeviceGuids.loadFile((String) null));
+
+        File copiedFile = copyResource("valid.json");
+        assertDoesNotThrow(() -> JsonDeviceGuids.loadFile(copiedFile));
+
+        String copiedPath = copiedFile.getAbsolutePath();
+        assertDoesNotThrow(() -> JsonDeviceGuids.loadFile(copiedPath));
+
+        /* ensure file is deleted before exiting */
+        assertTrue(copiedFile.delete());
+    }
+
+}
