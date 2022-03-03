b1424c949a26d4e70b14ea752b0d8ac535a12bde
Whirvis Ardenaur <whirvis@ardenus.org>
2022-03-02 21:56:55 -0600

    Update MockController.java

--- devices/controller/src/test/java/io/ketill/controller/MockController.java
+++ devices/controller/src/test/java/io/ketill/controller/MockController.java
@@ -8,14 +8,6 @@ class MockController extends Controller {
 
     MockController(@NotNull AdapterSupplier<?> adapterSupplier,
                    @Nullable AnalogStick ls, @Nullable AnalogStick rs,
-                   @Nullable AnalogTrigger lt, @Nullable AnalogTrigger rt,
-                   boolean registerFields, boolean initAdapter) {
-        super("mock_controller", adapterSupplier, ls, rs, lt, rt,
-                registerFields, initAdapter);
-    }
-
-    MockController(@NotNull AdapterSupplier<?> adapterSupplier,
-                   @Nullable AnalogStick ls, @Nullable AnalogStick rs,
                    @Nullable AnalogTrigger lt, @Nullable AnalogTrigger rt) {
         super("mock_controller", adapterSupplier, ls, rs, lt, rt);
     }
