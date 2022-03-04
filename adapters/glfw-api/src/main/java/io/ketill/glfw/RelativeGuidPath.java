a1d74d8acac844f33d6b3eab67e754f65f8d2610
Whirvis Ardenaur <whirvis@ardenus.org>
2022-03-04 16:00:27 -0600

    Rename GuidResourcePath to RelativeGuidPath

--- adapters/glfw-api/src/main/java/io/ketill/glfw/RelativeGuidPath.java
+++ adapters/glfw-api/src/main/java/io/ketill/glfw/RelativeGuidPath.java
@@ -1,18 +1,22 @@
 package io.ketill.glfw;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
  * When present, signals to a {@link GlfwJoystickSeeker} it should use a
- * different base path when loading GUID resources. By default, the base
- * path is the package in which the GLFW joystick seeker resides.
+ * different relative base path when loading GUID resources. By default,
+ * the base path is the package in which the GLFW joystick seeker resides.
  *
  * @see #ROOT
  * @see GlfwJoystickSeeker#loadJsonGuids(String)
  */
+@Documented
+@Inherited
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface GuidResourcePath {
