4ce9a359ee82b9579851686689c384848df3a114
Whirvis Ardenaur <whirvis@ardenus.org>
2022-03-06 11:16:22 -0600

    Add missing docs to MappingType in API module

--- api/src/main/java/io/ketill/MappingType.java
+++ api/src/main/java/io/ketill/MappingType.java
@@ -7,7 +7,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- *
+ * When present, indicates a type is used solely for mapping a feature in a
+ * {@link MappedFeatureRegistry}. These exist for features that require more
+ * than one data field to map (e.g., an analog stick). Use of this annotation
+ * is optional, but recommended as it improves code readability.
  */
 @Documented
 @Target(ElementType.TYPE)
