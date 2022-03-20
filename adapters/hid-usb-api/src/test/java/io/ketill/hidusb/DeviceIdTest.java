fae8222f70a73a0dbdcf17e4a27b29570e9a61b0
Whirvis Ardenaur <whirvis@ardenus.org>
2022-03-20 14:12:45 -0500

    Create DeviceId in hid-usb-api module

--- adapters/hid-usb-api/src/test/java/io/ketill/hidusb/DeviceIdTest.java
+++ adapters/hid-usb-api/src/test/java/io/ketill/hidusb/DeviceIdTest.java
@@ -2,11 +2,13 @@ package io.ketill.hidusb;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DeviceInfoTest {
+@SuppressWarnings("ConstantConditions")
+class DeviceIdTest {
 
     @Test
     void __init__() {
@@ -37,6 +39,56 @@ class DeviceInfoTest {
         int vendorId = random.nextInt(0xFFFF);
         int productId = random.nextInt(0xFFFF);
         assertDoesNotThrow(() -> new DeviceId(vendorId, productId));
+    }
+
+    @Test
+    void testEquals() {
+        DeviceId id = new DeviceId(0x1234, 0x5678);
+
+        /*
+         * TODO
+         */
+        boolean equalsNull = id.equals(null);
+        assertFalse(equalsNull);
+
+        /*
+         * TODO
+         */
+        Object obj = new Object();
+        boolean equalsObj = id.equals(obj);
+        assertFalse(equalsObj);
+
+        /*
+         * TODO
+         */
+        DeviceId blank = new DeviceId(0x0000, 0x0000);
+        boolean equalsBlank = id.equals(blank);
+        assertFalse(equalsBlank);
+
+        /*
+         * TODO
+         */
+        @SuppressWarnings("EqualsWithItself")
+        boolean equalsSelf = id.equals(id);
+        assertTrue(equalsSelf);
+
+        /*
+         * TODO
+         */
+        DeviceId copy = new DeviceId(id.vendorId, id.productId);
+        boolean equalsCopy = id.equals(copy);
+        assertTrue(equalsCopy);
+    }
+
+    @Test
+    void testHashCode() {
+        Random random = new Random();
+        int vendorId = random.nextInt(0xFFFF);
+        int productId = random.nextInt(0xFFFF);
+        DeviceId id = new DeviceId(vendorId, productId);
+
+        int hash = Objects.hash(vendorId, productId);
+        assertEquals(hash, id.hashCode());
     }
 
 }
