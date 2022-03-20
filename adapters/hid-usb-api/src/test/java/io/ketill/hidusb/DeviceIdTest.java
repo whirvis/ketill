02842ca52c32fd4bc3999d2d7ad0528244300e3e
Whirvis Ardenaur <whirvis@ardenus.org>
2022-03-20 14:12:07 -0500

    Being redoing hid-usb-api module

--- adapters/hid-usb-api/src/test/java/io/ketill/hidusb/DeviceIdTest.java
+++ adapters/hid-usb-api/src/test/java/io/ketill/hidusb/DeviceIdTest.java
@@ -15,18 +15,18 @@ class DeviceInfoTest {
          * the range of 0x0000 to 0xFFFF is out of bounds.
          */
         assertThrows(IllegalArgumentException.class,
-                () -> new DeviceInfo(Integer.MIN_VALUE, 0x0000));
+                () -> new DeviceId(Integer.MIN_VALUE, 0x0000));
         assertThrows(IllegalArgumentException.class,
-                () -> new DeviceInfo(Integer.MAX_VALUE, 0x0000));
+                () -> new DeviceId(Integer.MAX_VALUE, 0x0000));
 
         /*
          * Product IDs are an unsigned short. Any value outside
          * the range of 0x0000 to 0xFFFF is out of bounds.
          */
         assertThrows(IllegalArgumentException.class,
-                () -> new DeviceInfo(0x0000, Integer.MIN_VALUE));
+                () -> new DeviceId(0x0000, Integer.MIN_VALUE));
         assertThrows(IllegalArgumentException.class,
-                () -> new DeviceInfo(0x0000, Integer.MAX_VALUE));
+                () -> new DeviceId(0x0000, Integer.MAX_VALUE));
 
         /*
          * Any randomly generated number within a range 0x0000 to
@@ -36,7 +36,7 @@ class DeviceInfoTest {
         Random random = new Random();
         int vendorId = random.nextInt(0xFFFF);
         int productId = random.nextInt(0xFFFF);
-        assertDoesNotThrow(() -> new DeviceInfo(vendorId, productId));
+        assertDoesNotThrow(() -> new DeviceId(vendorId, productId));
     }
 
 }
