c39b6a927b69d6e2aefea8bba2f20ce15e825e05
Whirvis Ardenaur <whirvis@ardenus.org>
2022-04-04 22:46:19 -0500

    Add IDs to Direction in API module

--- api/src/main/java/io/ketill/Direction.java
+++ api/src/main/java/io/ketill/Direction.java
@@ -1,8 +1,10 @@
 package io.ketill;
 
+import org.jetbrains.annotations.NotNull;
+
 public enum Direction {
 
-    UP, DOWN, LEFT, RIGHT;
+    UP(0), DOWN(1), LEFT(2), RIGHT(3);
 
     /**
      * Aliases for {@link #UP}, {@link #DOWN}, {@link #LEFT}, and
@@ -13,5 +15,29 @@ public enum Direction {
             NORTH = UP, SOUTH = DOWN,
             WEST = LEFT, EAST = RIGHT;
     /* @formatter:on */
+
+    public final int id;
+
+    Direction(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @param id the event ID.
+     * @return the {@code Direction} with the specified ID.
+     * @throws IllegalArgumentException if no such direction exists.
+     */
+    public static @NotNull Direction fromId(int id) {
+        for (Direction value : values()) {
+            if (value.id == id) {
+                return value;
+            }
+        }
+
+        String msg = "no such ";
+        msg += Direction.class.getSimpleName();
+        msg += " with ID " + id;
+        throw new IllegalArgumentException(msg);
+    }
 
 }
--- api/src/test/java/io/ketill/DirectionTest.java
+++ api/src/test/java/io/ketill/DirectionTest.java
@@ -14,4 +14,25 @@ class DirectionTest {
         assertSame(Direction.EAST, Direction.RIGHT);
     }
 
+    @Test
+    void fromId() {
+        /*
+         * It would not make sense to get a direction from
+         * an ID for a direction that does not exist. Assume
+         * this was a user mistake by the user and throw an
+         * exception.
+         */
+        assertThrows(IllegalArgumentException.class,
+                () -> Direction.fromId(-1));
+
+        /*
+         * It would not make sense for fromId() to return a
+         * different Direction than the one requested. This
+         * is more or less a sanity check.
+         */
+        for (Direction value : Direction.values()) {
+            assertSame(value, Direction.fromId(value.id));
+        }
+    }
+
 }
