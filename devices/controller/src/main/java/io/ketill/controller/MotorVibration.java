41007e850347ddef6004a99a2c196015302618cf
Whirvis Ardenaur <whirvis@ardenus.org>
2022-04-29 13:21:40 -0500

    Implement MotorVibrationTest

--- devices/controller/src/main/java/io/ketill/controller/MotorVibration.java
+++ devices/controller/src/main/java/io/ketill/controller/MotorVibration.java
@@ -10,6 +10,14 @@ public class MotorVibration {
     private float strength;
 
     /**
+     * @return the current vibration strength, guaranteed to be in range of
+     * {@code 0.0F} to {@code 1.0F}.
+     */
+    public float getStrength() {
+        return this.strength;
+    }
+
+    /**
      * @param strength the vibration strength to use. This value will be
      *                 capped to a range of {@code 0.0F} to {@code 1.0F}.
      * @return this vibration state.
@@ -18,14 +26,6 @@ public class MotorVibration {
         /* guarantee a range of 0.0F to 1.0F */
         this.strength = Math.max(0.0F, Math.min(1.0F, strength));
         return this;
-    }
-
-    /**
-     * @return the current vibration strength, guaranteed to be in range of
-     * {@code 0.0F} to {@code 1.0F}.
-     */
-    public float getStrength() {
-        return this.strength;
     }
 
 }
