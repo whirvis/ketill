1f4a94763f22cc59e23456f62ffd184bf23ba91c
Whirvis Ardenaur <whirvis@ardenus.org>
2022-04-29 16:58:52 -0500

    Re-add KeyboardKeyMonitorTest and MouseButtonMonitorTest

---
+++ devices/pc/src/test/java/io/ketill/pc/KeyboardKeyMonitorTest.java
@@ -0,0 +1,73 @@
+package io.ketill.pc;
+
+import io.ketill.pressable.PressableFeatureEvent;
+import io.ketill.pressable.PressableFeatureEventType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class KeyboardKeyMonitorTest {
+
+    private KeyPressZ state;
+    private KeyboardKeyMonitor monitor;
+
+    @BeforeEach
+    void setup() {
+        Keyboard keyboard = mock(Keyboard.class);
+        KeyboardKey key = new KeyboardKey("key");
+        this.state = new KeyPressZ();
+
+        /*
+         * These mock the functionality required for the pressable feature
+         * monitor to instantiate without any exceptions being thrown. At
+         * construction, the monitor ensures the feature is registered and
+         * that it owns the state supplied by the user.
+         */
+        doReturn(true).when(keyboard).isFeatureRegistered(key);
+        doReturn(key).when(keyboard).getFeature(state);
+
+        this.monitor = new KeyboardKeyMonitor(keyboard, key, state,
+                () -> null);
+    }
+
+    private void fireSimulatedEvent(PressableFeatureEventType type) {
+        PressableFeatureEvent event = new PressableFeatureEvent(type,
+                monitor.device, monitor.feature, false, null);
+        monitor.eventFired(event);
+    }
+
+    @Test
+    void eventFired() {
+        /*
+         * When the HOLD event is fired, the keyboard key monitor must
+         * set the state of the key it was assigned to be held down.
+         */
+        fireSimulatedEvent(PressableFeatureEventType.HOLD);
+        assertTrue(state.held);
+
+        /*
+         * When the RELEASE event is fired, the keyboard key monitor must
+         * set the state of the key it was assigned to not be held down.
+         */
+        fireSimulatedEvent(PressableFeatureEventType.RELEASE);
+        assertFalse(state.held);
+    }
+
+    /*
+     * For some reason, IntelliJ says that the code below has a redundant
+     * assignment. Furthermore, it says monitor.isPressed() is simplifiable
+     * to true. These warnings are both wrong (IDE bug perhaps?). As such,
+     * I had them suppressed.
+     */
+    @SuppressWarnings({"ConstantConditions", "UnusedAssignment"})
+    @Test
+    void isPressed() {
+        state.pressed = true;
+        assertTrue(monitor.isPressed());
+        state.pressed = false;
+        assertFalse(monitor.isPressed());
+    }
+
+}
