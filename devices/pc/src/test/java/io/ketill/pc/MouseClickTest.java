280b27703efdea3ab455c353a15e981e63b00e7c
Whirvis Ardenaur <whirvis@ardenus.org>
2022-05-25 16:29:31 -0500

    Fix broken MouseClickTest

--- devices/pc/src/test/java/io/ketill/pc/MouseClickTest.java
+++ devices/pc/src/test/java/io/ketill/pc/MouseClickTest.java
@@ -1,28 +1,31 @@
 package io.ketill.pc;
 
+import io.ketill.IoDeviceObserver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static io.ketill.pc.EventAssertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-@SuppressWarnings("ConstantConditions")
 class MouseClickTest {
 
+    private IoDeviceObserver observer;
     private MouseClickZ internal;
     private MouseClick container;
 
     @BeforeEach
     void createState() {
-        this.internal = new MouseClickZ(null, null);
+        Mouse mouse = mock(Mouse.class);
+        this.observer = mock(IoDeviceObserver.class);
+        when(observer.getDevice()).thenReturn(mouse);
+
+        MouseButton button = new MouseButton("button");
+        this.internal = new MouseClickZ(button, observer);
         this.container = new MouseClick(internal);
     }
 
-    @Test
-    void testInit() {
-        assertThrows(NullPointerException.class,
-                () -> new MouseClick(null));
-    }
-
+    @SuppressWarnings({"ConstantConditions", "UnusedAssignment"})
     @Test
     void testIsPressed() {
         internal.pressed = true;
@@ -31,6 +34,7 @@ class MouseClickTest {
         assertFalse(container.isPressed());
     }
 
+    @SuppressWarnings({"ConstantConditions", "UnusedAssignment"})
     @Test
     void testIsHeld() {
         internal.held = true;
@@ -38,5 +42,17 @@ class MouseClickTest {
         internal.held = false;
         assertFalse(container.isHeld());
     }
+
+    @Test
+    void testUpdate() {
+        internal.pressed = true;
+        internal.update(); /* trigger event emission */
+        assertEmitted(observer, MouseButtonPressEvent.class);
+
+        internal.pressed = false; /* release trigger */
+        internal.update(); /* trigger event emission */
+        assertEmitted(observer, MouseButtonReleaseEvent.class);
+    }
+
 
 }
