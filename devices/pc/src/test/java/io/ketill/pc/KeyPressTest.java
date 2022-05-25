f417325b7197cd897c677f92aca8380ae540ff2a
Whirvis Ardenaur <whirvis@ardenus.org>
2022-05-25 16:43:32 -0500

    Add missing tests to KeyPressTest

--- devices/pc/src/test/java/io/ketill/pc/KeyPressTest.java
+++ devices/pc/src/test/java/io/ketill/pc/KeyPressTest.java
@@ -1,28 +1,31 @@
 package io.ketill.pc;
 
+import io.ketill.IoDeviceObserver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static io.ketill.pc.EventAssertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-@SuppressWarnings("ConstantConditions")
 class KeyPressTest {
 
+    private IoDeviceObserver observer;
     private KeyPressZ internal;
     private KeyPress container;
 
     @BeforeEach
     void createState() {
-        this.internal = new KeyPressZ(null, null);
+        Keyboard keyboard = mock(Keyboard.class);
+        this.observer = mock(IoDeviceObserver.class);
+        when(observer.getDevice()).thenReturn(keyboard);
+
+        KeyboardKey key = new KeyboardKey("key");
+        this.internal = new KeyPressZ(key, observer);
         this.container = new KeyPress(internal);
     }
 
-    @Test
-    void testInit() {
-        assertThrows(NullPointerException.class,
-                () -> new KeyPress(null));
-    }
-
+    @SuppressWarnings({"ConstantConditions", "UnusedAssignment"})
     @Test
     void testIsPressed() {
         internal.pressed = true;
@@ -31,12 +34,24 @@ class KeyPressTest {
         assertFalse(container.isPressed());
     }
 
+    @SuppressWarnings({"ConstantConditions", "UnusedAssignment"})
     @Test
     void testIsHeld() {
         internal.held = true;
         assertTrue(container.isHeld());
         internal.held = false;
         assertFalse(container.isHeld());
+    }
+
+    @Test
+    void testUpdate() {
+        internal.pressed = true; /* press key */
+        internal.update(); /* trigger event emission */
+        assertEmitted(observer, KeyboardKeyPressEvent.class);
+
+        internal.pressed = false; /* release key */
+        internal.update(); /* trigger event emission */
+        assertEmitted(observer, KeyboardKeyReleaseEvent.class);
     }
 
 }
