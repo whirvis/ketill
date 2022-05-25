56e0ab4978badc88dad21e1633a8b83d99ebc347
Whirvis Ardenaur <whirvis@ardenus.org>
2022-05-25 16:15:29 -0500

    Add missing test to MouseButtonTest

--- devices/pc/src/test/java/io/ketill/pc/MouseButtonTest.java
+++ devices/pc/src/test/java/io/ketill/pc/MouseButtonTest.java
@@ -1,31 +1,36 @@
 package io.ketill.pc;
 
+import io.ketill.IoDeviceObserver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-@SuppressWarnings("ConstantConditions")
 class MouseButtonTest {
 
-    private MouseButton key;
+    private MouseButton button;
 
     @BeforeEach
     void createButton() {
-        this.key = new MouseButton("button");
+        this.button = new MouseButton("button");
     }
 
     @Test
-    void testInit() {
-        assertThrows(NullPointerException.class,
-                () -> new MouseButton(null));
+    void testGetDevice() {
+        assertSame(Mouse.class, button.getDeviceType());
     }
 
     @Test
     void testGetState() {
-        MouseClickZ internal = key.getInternalState(null);
+        Mouse mouse = mock(Mouse.class);
+        IoDeviceObserver observer = mock(IoDeviceObserver.class);
+        when(observer.getDevice()).thenReturn(mouse);
+
+        MouseClickZ internal = button.getInternalState(observer);
         assertNotNull(internal);
-        MouseClick container = key.getContainerState(internal);
+
+        MouseClick container = button.getContainerState(internal);
         assertNotNull(container);
     }
 
