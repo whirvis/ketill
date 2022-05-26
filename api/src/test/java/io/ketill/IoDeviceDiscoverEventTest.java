2c48aa4a5875511531947f7ab9a16964c595ebb0
Whirvis Ardenaur <whirvis@ardenus.org>
2022-05-26 18:31:10 -0500

    Fix broken tests in API module

--- api/src/test/java/io/ketill/IoDeviceDiscoverEventTest.java
+++ api/src/test/java/io/ketill/IoDeviceDiscoverEventTest.java
@@ -6,24 +6,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@SuppressWarnings("ConstantConditions")
 class IoDeviceDiscoverEventTest {
 
-    private IoDeviceSeeker<?> seeker;
     private IoDevice device;
     private IoDeviceDiscoverEvent event;
 
     @BeforeEach
     void createEvent() {
-        this.seeker = mock(IoDeviceSeeker.class);
+        IoDeviceSeeker<?> seeker = mock(IoDeviceSeeker.class);
         this.device = mock(IoDevice.class);
         this.event = new IoDeviceDiscoverEvent(seeker, device);
-    }
-
-    @Test
-    void testInit() {
-        assertThrows(NullPointerException.class,
-                () -> new IoDeviceDiscoverEvent(seeker, null));
     }
 
     @Test
--- api/src/test/java/io/ketill/IoDeviceForgetEventTest.java
+++ api/src/test/java/io/ketill/IoDeviceForgetEventTest.java
@@ -6,24 +6,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@SuppressWarnings("ConstantConditions")
 class IoDeviceForgetEventTest {
 
-    private IoDeviceSeeker<?> seeker;
     private IoDevice device;
     private IoDeviceForgetEvent event;
 
     @BeforeEach
     void createEvent() {
-        this.seeker = mock(IoDeviceSeeker.class);
+        IoDeviceSeeker<?> seeker = mock(IoDeviceSeeker.class);
         this.device = mock(IoDevice.class);
         this.event = new IoDeviceForgetEvent(seeker, device);
-    }
-
-    @Test
-    void testInit() {
-        assertThrows(NullPointerException.class,
-                () -> new IoDeviceForgetEvent(seeker, null));
     }
 
     @Test
