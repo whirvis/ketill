074948c85e6cb4616076e9be3b2f778ff61e466b
Whirvis Ardenaur <whirvis@ardenus.org>
2022-06-03 16:23:39 -0500

    Update AutonomousField.java

--- api/src/main/java/io/ketill/AutonomousField.java
+++ api/src/main/java/io/ketill/AutonomousField.java
@@ -13,6 +13,8 @@ import java.lang.annotation.Target;
  * <p>
  * <b>Note:</b> Use of this annotation is optional. However, it is
  * recommended as it improves code readability.
+ *
+ * @see AutonomousState
  */
 @Documented
 @Target(ElementType.FIELD)
