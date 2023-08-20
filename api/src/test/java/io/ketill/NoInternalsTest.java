f1120f4599896e66b1ec037a1959763885b7855b
Whirvis Ardenaur <whirvis@ardenus.org>
2023-08-19 19:45:11 -0500

    Add NoInternals unit test

---
+++ api/src/test/java/io/ketill/NoInternalsTest.java
@@ -0,0 +1,27 @@
+package io.ketill;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Modifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public final class NoInternalsTest {
+
+    @Test
+    void ensureSingleton() {
+        Class<?> clazz = NoInternals.class;
+        assertTrue(Modifier.isFinal(clazz.getModifiers()),
+                clazz.getName() + " must be final");
+        assertEquals(0, clazz.getConstructors().length,
+                clazz.getName() + " must be inconstructible");
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("no internals",
+                NoInternals.INSTANCE.toString());
+    }
+
+}
