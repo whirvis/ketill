a71b21b6cab9c002fa62d72f6a660f9719455154
Whirvis Ardenaur <whirvis@ardenus.org>
2023-08-25 14:28:52 -0500

    Add extra functionality to IoMode

--- api/src/main/java/io/ketill/IoMode.java
+++ api/src/main/java/io/ketill/IoMode.java
@@ -1,32 +1,112 @@
 package io.ketill;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+
 /**
- * Determines when the state of an {@link IoFeature} is bridged.
- *
- * @see IoState
+ * TODO: docs
  */
 public enum IoMode {
 
     /**
-     * Obtain the {@link IoFeature}'s state on read.
-     * <p>
-     * Use this when the {@link IoState} stores data obtained from the device.
-     * Examples include (but are not limited to) if a gamepad button is pressed
-     * or the position of an analog stick.
-     *
-     * @see IoHandle#read()
+     * TODO: docs
      */
-    READ,
+    READ(true, false, "r"),
 
     /**
-     * Communicate the {@link IoFeature}'s state on write.
-     * <p>
-     * Use this when the {@link IoState} stores data communicated to the device.
-     * Examples include (but are not limited to) the intensity of a rumble motor
-     * or an LED's display number.
-     *
-     * @see IoHandle#write()
+     * TODO: docs
      */
-    WRITE;
+    WRITE(false, true, "w", "a"),
+
+    /**
+     * TODO: docs
+     */
+    READ_WRITE(true, true, "r+", "w+", "a+");
+
+    private final boolean read;
+    private final boolean write;
+    private final @NotNull List<@NotNull String> ids;
+
+    IoMode(boolean read, boolean write, @NotNull String @NotNull ... ids) {
+        this.read = read;
+        this.write = write;
+        this.ids = Arrays.asList(ids);
+    }
+
+    /**
+     * Returns if this I/O mode supports read operations.
+     *
+     * @return {@code true} if this I/O mode supports read operations,
+     * {@code false} otherwise.
+     * @see #supports(IoMode)
+     */
+    public boolean isRead() {
+        return this.read;
+    }
+
+    /**
+     * Returns if this I/O mode supports write operations.
+     *
+     * @return {@code true} if this I/O mode supports write operations,
+     * {@code false} otherwise.
+     * @see #supports(IoMode)
+     */
+    public boolean isWrite() {
+        return this.write;
+    }
+
+    /**
+     * Returns if this I/O mode supports the operations of another.
+     *
+     * @param mode the mode to compare.
+     * @return {@code true} if this I/O mode supports all the operations
+     * of {@code mode}, {@code false} otherwise.
+     * @see #isRead()
+     * @see #isWrite()
+     */
+    public boolean supports(@Nullable IoMode mode) {
+        if (mode == null) {
+            return false;
+        } else if (mode == this) {
+            return true;
+        } else {
+            return (this.isRead() || !mode.isRead())
+                    && (this.isWrite() || !mode.isWrite());
+        }
+    }
+
+    /**
+     * Returns the IDs for this I/O mode.
+     *
+     * @return the IDs for this I/O mode.
+     * @see #byId(String)
+     */
+    public @Nullable String @NotNull [] ids() {
+        return ids.toArray(new String[0]);
+    }
+
+    /**
+     * Returns the I/O mode with the specified ID. The string must match
+     * exactly an ID for one of the declared modes (extraneous whitespace
+     * is not permitted).
+     *
+     * @param id the mode ID.
+     * @return the I/O mode with the specified ID.
+     * @throws NullPointerException     if {@code id} is {@code null}.
+     * @throws IllegalArgumentException if there is no mode with the
+     *                                  specified ID.
+     * @see #ids()
+     */
+    public static @NotNull IoMode byId(@NotNull String id) {
+        Objects.requireNonNull(id, "id cannot be null");
+        for (IoMode value : IoMode.values()) {
+            if (value.ids.contains(id)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("No mode " + id);
+    }
 
 }
--- api/src/test/java/io/ketill/IoModeTest.java
+++ api/src/test/java/io/ketill/IoModeTest.java
@@ -2,15 +2,69 @@ package io.ketill;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
+@SuppressWarnings("ConstantConditions")
 public final class IoModeTest {
 
     @Test
-    void testVariants() {
-        /* This test only exists for coverage */
-        assertNotNull(IoMode.READ);
-        assertNotNull(IoMode.WRITE);
+    void testIsRead() {
+        assertTrue(IoMode.READ.isRead());
+        assertFalse(IoMode.WRITE.isRead());
+        assertTrue(IoMode.READ_WRITE.isRead());
+    }
+
+    @Test
+    void testIsWrite() {
+        assertFalse(IoMode.READ.isWrite());
+        assertTrue(IoMode.WRITE.isWrite());
+        assertTrue(IoMode.READ_WRITE.isWrite());
+    }
+
+    @Test
+    void testSupports() {
+        for (IoMode mode : IoMode.values()) {
+            assertFalse(mode.supports(null));
+        }
+
+        assertTrue(IoMode.READ.supports(IoMode.READ));
+        assertFalse(IoMode.READ.supports(IoMode.WRITE));
+        assertFalse(IoMode.READ.supports(IoMode.READ_WRITE));
+
+        assertFalse(IoMode.WRITE.supports(IoMode.READ));
+        assertTrue(IoMode.WRITE.supports(IoMode.WRITE));
+        assertFalse(IoMode.WRITE.supports(IoMode.READ_WRITE));
+
+        assertTrue(IoMode.READ_WRITE.supports(IoMode.READ));
+        assertTrue(IoMode.READ_WRITE.supports(IoMode.WRITE));
+        assertTrue(IoMode.READ_WRITE.supports(IoMode.READ_WRITE));
+    }
+
+    @Test
+    void testIds() {
+        assertArrayEquals(new String[]{"r"},
+                IoMode.READ.ids());
+        assertArrayEquals(new String[]{"w", "a"},
+                IoMode.WRITE.ids());
+        assertArrayEquals(new String[]{"r+", "w+", "a+"},
+                IoMode.READ_WRITE.ids());
+    }
+
+    @Test
+    void testGetById() {
+        assertThrows(NullPointerException.class,
+                () -> IoMode.byId(null));
+        assertThrows(IllegalArgumentException.class,
+                () -> IoMode.byId("rw"));
+
+        assertSame(IoMode.READ, IoMode.byId("r"));
+
+        assertSame(IoMode.WRITE, IoMode.byId("w"));
+        assertSame(IoMode.WRITE, IoMode.byId("a"));
+
+        assertSame(IoMode.READ_WRITE, IoMode.byId("r+"));
+        assertSame(IoMode.READ_WRITE, IoMode.byId("w+"));
+        assertSame(IoMode.READ_WRITE, IoMode.byId("a+"));
     }
 
 }
