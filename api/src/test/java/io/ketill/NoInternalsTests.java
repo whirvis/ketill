63dffeaf3c634077b22efe9cd2c7a706ba34aacd
Whirvis Ardenaur <whirvis@ardenus.org>
2023-09-03 21:24:13 -0500

    Tweak NoInternalsTests naming

--- api/src/test/java/io/ketill/NoInternalsTests.java
+++ api/src/test/java/io/ketill/NoInternalsTests.java
@@ -3,36 +3,42 @@ package io.ketill;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public final class NoInternalsTest {
+public final class NoInternalsTests {
 
     private static Class<?> clazz;
+    private static Method toStringMethod;
+    private static Field instanceField;
 
     @BeforeAll
-    static void launch() {
+    static void launch() throws NoSuchMethodException, NoSuchFieldException {
         clazz = NoInternals.class;
+        toStringMethod = clazz.getDeclaredMethod("toString");
+        instanceField = clazz.getDeclaredField("INSTANCE");
     }
 
     @Test
-    void ensureSingleton() {
+    void classIsSingleton() {
         assertTrue(Modifier.isFinal(clazz.getModifiers()),
                 clazz.getName() + " must be final");
         assertEquals(0, clazz.getConstructors().length,
                 clazz.getName() + " must be inconstructible");
+
+        assertTrue(Modifier.isStatic(instanceField.getModifiers()),
+                instanceField.getName() + " must be static");
+        assertTrue(Modifier.isFinal(instanceField.getModifiers()),
+                instanceField.getName() + " must be final");
     }
 
     @Test
-    void ensureToStringImplemented() throws NoSuchMethodException {
-        Method toString = clazz.getMethod("toString");
-        assertEquals(clazz, toString.getDeclaringClass());
-    }
-
-    @Test
-    void testToString() {
+    void toStringIsOverridden() {
+        assertEquals(clazz, toStringMethod.getDeclaringClass(),
+                toStringMethod.getName() + " must be overridden");
         assertNotNull(NoInternals.INSTANCE.toString());
     }
 
