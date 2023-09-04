1288ca69a69277192b8c13bf9df8c413e0f7e996
Whirvis Ardenaur <whirvis@ardenus.org>
2023-09-03 21:38:18 -0500

    Refactor IoType unit tests

--- api/src/test/java/io/ketill/IoTypeTests.java
+++ api/src/test/java/io/ketill/IoTypeTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public final class IoTypeTest {
+public final class IoTypeTests {
 
     @Test
-    void testAliases() {
+    void aliasesMatch() {
         assertEquals(IoType.INPUT, IoType.IN);
         assertEquals(IoType.OUTPUT, IoType.OUT);
     }
