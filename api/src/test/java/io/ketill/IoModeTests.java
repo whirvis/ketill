eae35827a23a041b6e9eeb7ebc26b770020421a6
Whirvis Ardenaur <whirvis@ardenus.org>
2023-09-09 23:20:08 -0500

    Improve IoMode unit tests

--- api/src/test/java/io/ketill/IoModeTests.java
+++ api/src/test/java/io/ketill/IoModeTests.java
@@ -2,24 +2,49 @@ package io.ketill;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SuppressWarnings("ConstantConditions")
-public final class IoModeTest {
+public final class IoModeTests {
 
     @Test
-    void capabilitiesAreAsExpected() {
-        assertTrue(IoMode.READ.isRead());
-        assertFalse(IoMode.WRITE.isRead());
-        assertTrue(IoMode.READ_WRITE.isRead());
+    void modesAreNotUseless() {
+        for (IoMode value : IoMode.values()) {
+            assertTrue(value.isRead() || value.isWrite());
+        }
+    }
 
-        assertFalse(IoMode.READ.isWrite());
+    @Test
+    void readModesSupportsRead() {
+        assertTrue(IoMode.READ.isRead());
+        assertTrue(IoMode.READ_WRITE.isRead());
+    }
+
+    @Test
+    void readOnlyModesDoNotSupportWrite() {
+        assertFalse(IoMode.READ.supports(IoMode.WRITE));
+        assertFalse(IoMode.READ.supports(IoMode.READ_WRITE));
+    }
+
+    @Test
+    void writeModesSupportsWrite() {
         assertTrue(IoMode.WRITE.isWrite());
         assertTrue(IoMode.READ_WRITE.isWrite());
     }
 
     @Test
-    void modesDontSupportNull() {
+    void writeOnlyModesDoNotSupportRead() {
+        assertFalse(IoMode.WRITE.supports(IoMode.READ));
+        assertFalse(IoMode.WRITE.supports(IoMode.READ_WRITE));
+    }
+
+    @Test
+    void modesDoNotSupportNull() {
         for (IoMode mode : IoMode.values()) {
             assertFalse(mode.supports(null));
         }
@@ -33,57 +58,56 @@ public final class IoModeTest {
     }
 
     @Test
-    void readDoesntSupportWrite() {
-        assertFalse(IoMode.READ.supports(IoMode.WRITE));
-        assertFalse(IoMode.READ.supports(IoMode.READ_WRITE));
+    void modesHaveAtLeastOneId() {
+        for (IoMode value : IoMode.values()) {
+            assertTrue(value.ids().length > 0);
+        }
     }
 
     @Test
-    void writeDoesntSupportRead() {
-        assertFalse(IoMode.WRITE.supports(IoMode.READ));
-        assertFalse(IoMode.WRITE.supports(IoMode.READ_WRITE));
+    void idsAreNotNull() {
+        for (IoMode value : IoMode.values()) {
+            for (String id : value.ids()) {
+                assertNotNull(id);
+            }
+        }
     }
 
     @Test
-    void readWriteSupportsBoth() {
-        assertTrue(IoMode.READ_WRITE.supports(IoMode.READ));
-        assertTrue(IoMode.READ_WRITE.supports(IoMode.WRITE));
+    void idsHaveNoWhitespace() {
+        Pattern nonWhitespacePattern = Pattern.compile("\\S+");
+        for (IoMode value : IoMode.values()) {
+            for (String id : value.ids()) {
+                Matcher matcher = nonWhitespacePattern.matcher(id);
+                assertTrue(matcher.matches(), "IDs must have no whitespace");
+            }
+        }
     }
 
     @Test
-    void testIds() {
-        assertArrayEquals(
-                new String[]{"r", "rb"},
-                IoMode.READ.ids());
-        assertArrayEquals(
-                new String[]{"w", "wb", "a", "ab"},
-                IoMode.WRITE.ids());
-        assertArrayEquals(
-                new String[]{"r+", "rb+", "w+", "wb+", "a+", "ab+"},
-                IoMode.READ_WRITE.ids());
+    void idsAreNotBlank() {
+        for (IoMode value : IoMode.values()) {
+            for (String id : value.ids()) {
+                assertFalse(id.trim().isEmpty(), "IDs must not be blank");
+            }
+        }
     }
 
     @Test
-    void testOf() {
-        assertThrows(NullPointerException.class,
-                () -> IoMode.of(null));
-        assertThrows(IllegalArgumentException.class,
-                () -> IoMode.of("rw"));
+    void idsAreUniqueAndConsistent() {
+        Set<String> ids = new HashSet<>();
+        for (IoMode value : IoMode.values()) {
+            for (String id : value.ids()) {
+                assertTrue(ids.add(id), "IDs must be unique");
+                assertSame(value, IoMode.of(id));
+            }
+        }
+    }
 
-        assertSame(IoMode.READ, IoMode.of("r"));
-        assertSame(IoMode.READ, IoMode.of("rb"));
-
-        assertSame(IoMode.WRITE, IoMode.of("w"));
-        assertSame(IoMode.WRITE, IoMode.of("wb"));
-        assertSame(IoMode.WRITE, IoMode.of("a"));
-        assertSame(IoMode.WRITE, IoMode.of("ab"));
-
-        assertSame(IoMode.READ_WRITE, IoMode.of("r+"));
-        assertSame(IoMode.READ_WRITE, IoMode.of("rb+"));
-        assertSame(IoMode.READ_WRITE, IoMode.of("w+"));
-        assertSame(IoMode.READ_WRITE, IoMode.of("wb+"));
-        assertSame(IoMode.READ_WRITE, IoMode.of("a+"));
-        assertSame(IoMode.READ_WRITE, IoMode.of("ab+"));
+    @Test
+    void illegalIdsCauseExceptions() {
+        assertThrows(NullPointerException.class, () -> IoMode.of(null));
+        assertThrows(IllegalArgumentException.class, () -> IoMode.of(""));
     }
 
 }
